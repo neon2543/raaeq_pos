d976ec405551647848ef50ef4bb04ce44a10a449
neon2543 <assaduljaman58@gmail.com>
2025-05-20 12:02:26 +0600

    Initial commit

---
+++ database/migrations/2023_12_22_064744_create_main_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('main_products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('code');
+            $table->unsignedBigInteger('product_unit');
+            $table->string('product_type')->default('Single');
+            $table->timestamps();
+        });
+    }
+    
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('main_products');
+    }
+};
